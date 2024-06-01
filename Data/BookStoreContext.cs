9f08df576530c63e375d6aad40cd27970855ba29
Miguel Angel Velazco Martinez <mvelazco@sparefoot.com>
2024-06-01 18:23:28 -0500

    SQL Server Configuration:   - Adding connection settings   - Installing additional Entity Framework dependencies   - Adding Models and DB Context class

---
+++ Data/BookStoreContext.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStoreApp.Data;
+
+public partial class BookStoreContext : DbContext
+{
+    public BookStoreContext()
+    {
+    }
+
+    public BookStoreContext(DbContextOptions<BookStoreContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Author> Authors { get; set; }
+
+    public virtual DbSet<Book> Books { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Author>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Authors__3214EC0734B19625");
+
+            entity.Property(e => e.Bio).HasMaxLength(250);
+            entity.Property(e => e.FirstName).HasMaxLength(50);
+            entity.Property(e => e.LastName).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Book>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Books__3214EC07763D2F68");
+
+            entity.HasIndex(e => e.Isbn, "UQ__Books__447D36EAA0901A5B").IsUnique();
+
+            entity.HasIndex(e => e.Summary, "UQ__Books__448AC82528A07C84").IsUnique();
+
+            entity.Property(e => e.Image).HasMaxLength(50);
+            entity.Property(e => e.Isbn)
+                .HasMaxLength(50)
+                .HasColumnName("ISBN");
+            entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.Summary).HasMaxLength(50);
+            entity.Property(e => e.Title).HasMaxLength(50);
+
+            entity.HasOne(d => d.Author).WithMany(p => p.Books)
+                .HasForeignKey(d => d.AuthorId)
+                .HasConstraintName("FK_Books_ToAuthors");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
